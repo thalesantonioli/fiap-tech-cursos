7021f195448bdf672fb015ccdda2ca46ce414b92
Henrique Vergara <henrique.vergara.hv@gmail.com>
2021-10-16 17:42:11 -0300

    Retirando imports nao utilizados

--- src/main/java/com/curso/fiap/curso/fiap/model/mapper/CursoMapper.java
+++ src/main/java/com/curso/fiap/curso/fiap/model/mapper/CursoMapper.java
@@ -3,12 +3,11 @@ package com.curso.fiap.curso.fiap.model.mapper;
 import java.util.List;
 import org.mapstruct.Mapper;
 import com.curso.fiap.curso.fiap.model.dto.CursoResponseDTO;
-import com.curso.fiap.curso.fiap.model.dto.ModuloResponseDTO;
 import com.curso.fiap.curso.fiap.model.entity.Curso;
-import com.curso.fiap.curso.fiap.model.entity.Modulo;
 
 @Mapper(componentModel = "spring")
 public interface CursoMapper {
   CursoResponseDTO cursoToCursoResponseDTO(Curso curso);
+
   List<CursoResponseDTO> cursoListToCursoResponseDTOList(List<Curso> curso);
 }
